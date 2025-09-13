af78fbe9c10c05b5f059a2e9f32ba7a8f0deb83f
Sravani-creator2004 <sravanikandula27@gmail.com>
2025-09-13 18:03:57 +0930

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,45 @@
-self.addEventListener("install", event => {
-event.waitUntil(
-caches.open("calc-cache").then(cache => {
-return cache.addAll([
-
+const cacheName = "calculator-cache-v1";
+const cacheFiles = [
 "index.html",
 "style.css",
-  "manifest.json",
-  "icon-192.png";
-  "icon-512.png"
 
-]);
+"manifest.json",
+"icon-192.png",
+"icon-512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(cacheName).then(cache => {
+return cache.addAll(cacheFiles);
 })
 );
+self.skipWaiting();
 });
 
+// Activate Service Worker
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.map(key => {
+if (key !== cacheName) {
+return caches.delete(key);
+}
+})
+)
+)
+);
+self.clients.claim();
+});
+
+// Fetch files
 self.addEventListener("fetch", event => {
 event.respondWith(
 caches.match(event.request).then(response => {
 return response || fetch(event.request);
 })
 );
-
 });
+
